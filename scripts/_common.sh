6fa19a5abebd2ff3857298d1c94b467c18431045
yalh76 <yalh@yahoo.com>
2019-08-05 05:47:47 +0200

    add dependency

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="curl tar"
+pkg_dependencies="curl tar pkg-config"
 
 #=================================================
 # PERSONAL HELPERS
