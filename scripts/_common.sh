b7a7c926c77013c3d022e24e5d101f05ac2fcac6
yalh76 <yalh@yahoo.com>
2021-05-09 11:06:05 +0200

    Handle migration from bitwarden

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="curl tar pkg-config libssl-dev libc6-dev"
+pkg_dependencies="curl tar pkg-config libssl-dev libc6-dev at"
 
 #=================================================
 # PERSONAL HELPERS
