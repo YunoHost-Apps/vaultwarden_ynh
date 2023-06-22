c3feeb37a50a708af7c2710da7dd131ca6d36d99
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-06-22 09:23:53 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="libpq5"
-
 pkg_image="vaultwarden/server"
 
 #=================================================
