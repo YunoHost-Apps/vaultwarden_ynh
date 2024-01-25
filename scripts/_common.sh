0ab990f8a35939829158c1e8f86754baf53e9599
OniriCorpe <oniricorpe@disroot.org>
2024-01-25 19:10:21 +0100

    symbolic link to fix libssl.so.3 bug

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -26,6 +26,11 @@ _download_vaultwarden_from_docker() {
     mv -f "$install_dir/build/"{vaultwarden,web-vault} "$install_dir/live/"
     ynh_secure_remove --file="$install_dir/build"
 
+    # fixes the libssl.so.3 not found bug since libssl3 is not available on bullseye
+    if [ ! -f /usr/lib/x86_64-linux-gnu/libssl.so.3 ]; then
+        ln -s /usr/lib/x86_64-linux-gnu/libssl.so /usr/lib/x86_64-linux-gnu/libssl.so.3
+    fi
+
     chmod 750 "$install_dir"
     chmod -R o-rwx "$install_dir"
     chown -R $app:$app "$install_dir"
