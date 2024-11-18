a32026027d5c7be7cd0b5ace1a32f89db2fce2e0
loowiz <47942583+looowizz@users.noreply.github.com>
2024-11-18 00:09:36 +0000

    Add _download_vaultwarden_ldap_from_docker function to _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -31,3 +31,31 @@ _download_vaultwarden_from_docker() {
     chmod -R o-rwx "$install_dir"
     chown -R $app:$app "$install_dir"
 }
+
+_download_vaultwarden_ldap_from_docker() {
+    docker_image="vividboarder/vaultwarden_ldap"
+    debian=$(lsb_release --codename --short)
+    if [[ $debian = "bullseye" ]]; then
+        docker_version="$(ynh_app_upstream_version)"
+    elif [[ $debian = "bookworm" ]]; then
+        docker_version="$(ynh_app_upstream_version)-alpine"
+    fi
+
+    docker_arg=""
+    # Fixup for armhf
+    if [ "$YNH_ARCH" == "armhf" ]; then
+        docker_arg="--os_arch_variant=linux/arm/v7" # Does 
+    fi
+
+    ynh_docker_image_extract --dest_dir="$install_dir/build/ldap" --image_spec="$docker_image:$docker_version" $docker_arg
+
+    # Move files from the extract to the live directory
+    ynh_secure_remove --file="$install_dir/live/ldap"
+    mkdir -p "$install_dir/live/ldap"
+    mv -f "$install_dir/build/"{vaultwarden,web-vault} "$install_dir/live/ldap"
+    ynh_secure_remove --file="$install_dir/build/ldap"
+
+    chmod 750 "$install_dir/ldap"
+    chmod -R o-rwx "$install_dir/ldap"
+    chown -R $app:$app "$install_dir/ldap"
+}
