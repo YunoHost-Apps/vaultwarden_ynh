1167c624f83db0a2ea93c34a0b86ab83b2c8d547
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-08-17 20:30:05 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,8 +8,8 @@ sqlite3-to-postgresql() {
     ynh_print_info "Migrating SQLite3 to PostgreSQL database..."
 
     tmpdir="$(mktemp -d)"
-    ynh_config_add --template="bitwarden.load" --destination="$tmpdir/bitwarden.load"
+    ynh_config_add --template="vaultwarden.load" --destination="$tmpdir/vaultwarden.load"
 
-    pgloader "$tmpdir/bitwarden.load"
-    ynh_safe_rm "$tmpdir/bitwarden.load"
+    pgloader "$tmpdir/vaultwarden.load"
+    ynh_safe_rm "$tmpdir/vaultwarden.load"
 }
