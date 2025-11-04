757a65a88ddbf0b7dcbe199541590ac422aff0d4
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-11-04 22:39:19 +0100

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,13 +3,3 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
-
-sqlite3-to-postgresql() {
-    ynh_print_info "Migrating SQLite3 to PostgreSQL database..."
-
-    tmpdir="$(mktemp -d)"
-    ynh_config_add --template="vaultwarden.load" --destination="$tmpdir/vaultwarden.load"
-
-    pgloader "$tmpdir/vaultwarden.load"
-    ynh_safe_rm "$tmpdir/vaultwarden.load"
-}
